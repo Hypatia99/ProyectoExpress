319c5900eeadeec6262950c6c40428646e002744
Grecia Hypatiagizeh Duran Navarro <hypadn@gmail.com>
2023-04-28 09:31:07 -0600

    Manejo de rutas2

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express=require('express');
 var app=express();
 
-app.get ('/', function(req, res) {
-    res.send ('Hola Mundo!');
+app.get('/', function(req, res){
+    res.send('Una API basica desde Express! ');
+});
+
+app.get('/saludo', function(req, res){
+    res.send('Hola desde la API!');
+});
+
+app.get('/despedida', function(req, res){
+    res.send('Adios desde una API!');
 });
 
 app.listen(3000, function(){
     console.log('Una API desde express')
-});
-app.listen(3000, function(){
-    console.log('Hola desde la API')
-});
-app.listen(3000, function(){
-    console.log('Adios desde una API ')
 });
